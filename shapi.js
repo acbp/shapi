17aabd6fc21dc0a9275be23a76c8c7c52792d860
Alexandre Calil Blasizza Paravani <paravanimobile@gmail.com>
2021-06-27 00:16:55 -0300

    tratamento de parametros

--- shapi.js
+++ shapi.js
@@ -4,20 +4,25 @@ const url = require('url')
 const exec = util.promisify(require('child_process').exec)
 const server = http.createServer((q,r)=>{
     let data = `Error ${r.method}`;
-    const command = `${url.parse(q.url).pathname.slice(1)||"echo"}`;
-    data ='';
-    q.on('data',(c)=> data+=c )
-    q.on('end',async()=>{
-        try{
+    try{
+        const command = `${url.parse(q.url).pathname.slice(1)||"echo"}`;
+        data ='';
+        q.on('data',(c)=> data+=c )
+        q.on('end',async()=>{
             console.log(`Running ${command}:\n<${data}`)
-            data = JSON.parse(data)
+            data = data && JSON.parse(data) 
             console.log(`exec('${command} ${JSON.stringify(data)}')`)
-            data = await exec(`${command} ${Object.values(data).map(s=>s).join(' ')}`,{shell:true})
+            data = await exec(`${command} ${Object.values(data).map(s=>s).join(' ')}`,{shell:true}).catch(()=>'Error');
             if(data.stderr) throw new Error(data.stderr);
+
             console.log(`>${data.stdout}`)
-        }
-        catch(e){ console.error(e) }
-            r.end(JSON.stringify({data}))
-    })
+            data = `${data.stdout}`
+        })
+    }
+    catch(e){ 
+        console.error(e)
+        data = 'An error has ocurred';
+    }
+    r.end(data)
 })
 server.listen(1337,()=> console.log('[0.0.0.0:1337] running'))
