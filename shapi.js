5d0f524f3b645c4941109d3c1a9d8372f9fdc90b
Alexandre Calil Blasizza Paravani <paravanimobile@gmail.com>
2021-07-04 01:51:19 -0300

    correção na execução do comando

--- shapi.js
+++ shapi.js
@@ -5,25 +5,26 @@ const url = require('url')
 const exec = util.promisify(require('child_process').exec)
 const server = http.createServer((q,r)=>{
     let data = `Error ${r.method}`;
-    try{
-        const command = `${url.parse(q.url).pathname.slice(1)||"echo"}`;
-        data ='';
-        q.on('data',(c)=> data+=c )
-        q.on('end',async()=>{
+    const command = `${url.parse(q.url).pathname.slice(1)||"echo"}`;
+    data ='';
+    q.on('data',(c)=> data+=c )
+    q.on('end',async()=>{
+        try{
             if(env.LOG<6) console.log(`Running ${command}:\n<${data}`)
             data = data && JSON.parse(data) 
             if(env.LOG<6) console.log(`exec('${command} ${JSON.stringify(data)}')`)
             data = await exec(`${command} ${Object.values(data).map(s=>s).join(' ')}`,{shell:true}).catch(()=>'Error');
             if(data.stderr) throw new Error(data.stderr);
 
-            if(env.LOG<6)console.log(`>${data.stdout}`)
-            data = `${data.stdout}`
-        })
-    }
-    catch(e){ 
-        if(env.LOG>4)console.error(e)
-        data = 'An error has ocurred';
-    }
-    r.end(data)
+            //data = `"${data.stdout}"`
+            if(env.LOG<6)console.log(`>${JSON.stringify(data)}`)
+            r.end(JSON.stringify(data))
+        }
+        catch(e){ 
+            if(env.LOG>4)console.error(e)
+            data = 'An error has ocurred, expected a json.';
+            r.end(data);
+        }
+    })
 })
 server.listen(env.PORT ,()=> console.log(`[::${env.PORT}] running`))
